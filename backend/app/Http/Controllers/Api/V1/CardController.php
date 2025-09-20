a0cd316d431e4e6ab768fb03182b15a55bf69b55
Christian <felix.christian.reyes@gmail.com>
2025-09-21 00:22:54 +0800

    fix: controller layout + import fix

--- backend/app/Http/Controllers/Api/V1/CardController.php
+++ backend/app/Http/Controllers/Api/V1/CardController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api\V1;
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\Api\V1\Controller;
 
 class CardController extends Controller
 {
--- backend/app/Http/Controllers/Api/V1/DeckController.php
+++ backend/app/Http/Controllers/Api/V1/DeckController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api\V1;
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\Api\V1\Controller;
 use App\Models\Deck;
 
 class DeckController extends Controller
