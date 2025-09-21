1cd29fce9ed14b1ddd831f2680c2965e067af040
Christian <felix.christian.reyes@gmail.com>
2025-09-21 22:54:37 +0800

    feat: add card observer on boot

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Card;
+use App\Observers\CardObserver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Card::observe(CardObserver::class);
     }
 }
